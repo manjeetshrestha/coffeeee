1843e023b3decde5fbc541225296f20a3b0eb947
manjeetshrestha <manjeetenvy75@gmail.com>
2025-03-23 18:17:56 +0545

    Add global styls.

---
+++ src/global/styles.ts
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+export const gs = StyleSheet.create({
+  flex1: {
+    flex: 1,
+  },
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  justifySpaceBetween: {
+    justifyContent: 'space-between',
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  gap4: {
+    gap: 4,
+  },
+  gap8: {
+    gap: 8,
+  },
+  gap15: {
+    gap: 15,
+  },
+  gap5: {
+    gap: 5,
+  },
+  gap9: {
+    gap: 9,
+  },
+});
