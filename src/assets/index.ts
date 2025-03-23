6ed0e989534fa7a936fbb0bcd3f62cd228532d11
manjeetshrestha <manjeetenvy75@gmail.com>
2025-03-23 17:59:27 +0545

    Add image assets.

---
+++ src/assets/index.ts
@@ -0,0 +1,17 @@
+export const coffeeBg = require('./coffeeBg.jpg');
+export const googleLogo = require('./googleLogo.png');
+
+export const homeIcon = require('./icons/Home.png');
+export const notificationIcon = require('./icons/Notification.png');
+export const heartHalfFilledIcon = require('./icons/HeartHalfFilled.png');
+export const bagIcon = require('./icons/Bag.png');
+
+export const promoImage = require('./promoImage.png');
+export const searchIcon = require('./icons/search-normal.png');
+export const settingsIcon = require('./icons/setting-4.png');
+export const addIcon = require('./icons/add.png');
+export const starIcon = require('./icons/star.png');
+export const heartIcon = require('./icons/Heart.png');
+export const arrowLeft = require('./icons/arrowLeft.png');
+export const milkIcon = require('./icons/milk.png');
+export const beanIcon = require('./icons/bean.png');
